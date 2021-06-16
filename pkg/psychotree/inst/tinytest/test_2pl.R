02400f43846b1f7eb446529a6f8bc686b60d2810
rdebelak (Rudolf Debelak) <rudolf.debelak@psychologie.uzh.ch>
2021-06-16 13:42:06 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/prefmod@307 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/psychotree/inst/tinytest/test_2pl.R
+++ pkg/psychotree/inst/tinytest/test_2pl.R
@@ -2,17 +2,17 @@
 
 ## load data and fit model ##
 data("SPISA", package = "psychotree")
-fit_2PL <- pltree(spisa ~ age + gender, data = SPISA, itemtype = "2PL", maxit = 5000)
+fit_2PL <- pltree(spisa[,1:9] ~ age + gender, data = SPISA, itemtype = "2PL", maxit = 5000)
 # saveRDS(fit_2PL, "inst/tinytest/fit_2PL.RDS") #dont run - old test
 
 # dont run, creation of smaller tests:
-# test_2PL <- list()
-# test_2PL[[1]] <- coef(fit_2PL)
-# test_2PL[[2]] <- itempar(fit_2PL)
-# test_2PL[[3]] <- threshpar(fit_2PL)
-# test_2PL[[4]] <- guesspar(fit_2PL)
-# test_2PL[[5]] <- upperpar(fit_2PL)
-# saveRDS(test_2PL, "inst/tinytest/test_2PL.RDS")
+#test_2PL <- list()
+#test_2PL[[1]] <- coef(fit_2PL)
+#test_2PL[[2]] <- itempar(fit_2PL)
+#test_2PL[[3]] <- threshpar(fit_2PL)
+#test_2PL[[4]] <- guesspar(fit_2PL)
+#test_2PL[[5]] <- upperpar(fit_2PL)
+#saveRDS(test_2PL, "test_2PL.RDS")
 
 ## test methods
 expect_equal(coef(fit_2PL), readRDS("test_2PL.RDS")[[1]],
@@ -31,7 +31,7 @@ expect_equal(upperpar(fit_2PL), readRDS("test_2PL.RDS")[[5]],
 
 ## load data and fit model ##
 data("SPISA", package = "psychotree")
-fit_2PL_mg <- pltree(spisa ~ gender | age + gender, data = SPISA, itemtype = "2PL", maxit = 5000)
+fit_2PL_mg <- pltree(spisa[,1:9] ~ gender | age + gender, data = SPISA, itemtype = "2PL", maxit = 5000)
 # saveRDS(fit_2PL_mg, "inst/tinytest/fit_2PL_mg.RDS") #dont run
 
 # dont run, creation of smaller tests:
@@ -41,7 +41,7 @@ fit_2PL_mg <- pltree(spisa ~ gender | age + gender, data = SPISA, itemtype = "2P
 # test_2PL_mg[[3]] <- threshpar(fit_2PL_mg)
 # test_2PL_mg[[4]] <- guesspar(fit_2PL_mg)
 # test_2PL_mg[[5]] <- upperpar(fit_2PL_mg)
-# saveRDS(test_2PL_mg, "inst/tinytest/test_2PL_mg.RDS")
+# saveRDS(test_2PL_mg, "test_2PL_mg.RDS")
 
 ## test methods
 expect_equal(coef(fit_2PL_mg), readRDS("test_2PL_mg.RDS")[[1]],
