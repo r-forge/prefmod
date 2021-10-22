55dae704622cb71f640d6f6796eab736d2521cda
rdebelak (Rudolf Debelak) <rudolf.debelak@psychologie.uzh.ch>
2021-10-22 16:35:13 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/prefmod@311 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/psychotree/inst/tinytest/test_1pl.R
+++ pkg/psychotree/inst/tinytest/test_1pl.R
@@ -2,7 +2,7 @@
 
 ## load data and fit model ##
 data("SPISA", package = "psychotree")
-fit_1PL <- pltree(spisa ~ age, data = SPISA, type = "1PL", maxit = 5000)
+fit_1PL <- pltree(spisa[,1:9] ~ age, data = SPISA, type = "1PL", maxit = 5000)
 # saveRDS(fit_1PL, "inst/tinytest/fit_1PL.RDS") #dont run, old test
 # dont run, creation of smaller test:
 # test_1PL <- list()
@@ -29,7 +29,7 @@ expect_equal(upperpar(fit_1PL), readRDS("test_1PL.RDS")[[5]],
 
 ## load data and fit model ##
 data("SPISA", package = "psychotree")
-fit_1PL_mg <- pltree(spisa ~ gender | gender, data = SPISA, type = "1PL", maxit = 2000)
+fit_1PL_mg <- pltree(spisa[,1:9] ~ gender | gender, data = SPISA, type = "1PL", maxit = 2000)
 # saveRDS(fit_1PL_mg, "inst/tinytest/fit_1PL_mg.RDS") #dont run
 
 # dont run, creation of smaller tests:
@@ -42,9 +42,14 @@ fit_1PL_mg <- pltree(spisa ~ gender | gender, data = SPISA, type = "1PL", maxit 
 # saveRDS(test_1PL_mg, "inst/tinytest/test_1PL_mg.RDS")
 
 ## test methods
-expect_equal(coef(fit_1PL_mg), readRDS("test_1PL_mg.RDS")[[1]])
-expect_equal(itempar(fit_1PL_mg), readRDS("test_1PL_mg.RDS")[[2]])
-expect_equal(threshpar(fit_1PL_mg), readRDS("test_1PL_mg.RDS")[[3]])
-expect_equal(guesspar(fit_1PL_mg), readRDS("test_1PL_mg.RDS")[[4]])
-expect_equal(upperpar(fit_1PL_mg), readRDS("test_1PL_mg.RDS")[[5]])
+expect_equal(coef(fit_1PL_mg), readRDS("test_1PL_mg.RDS")[[1]],
+             tolerance = 0.0001)
+expect_equal(itempar(fit_1PL_mg), readRDS("test_1PL_mg.RDS")[[2]],
+             tolerance = 0.0001)
+expect_equal(threshpar(fit_1PL_mg), readRDS("test_1PL_mg.RDS")[[3]],
+             tolerance = 0.0001)
+expect_equal(guesspar(fit_1PL_mg), readRDS("test_1PL_mg.RDS")[[4]],
+             tolerance = 0.0001)
+expect_equal(upperpar(fit_1PL_mg), readRDS("test_1PL_mg.RDS")[[5]],
+             tolerance = 0.0001)
 
